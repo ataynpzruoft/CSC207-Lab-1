bf648e224f38d2dee9368095b26af3fefd3181e1
atayenipazar <ata.yenipazar@mail.utoronto.ca>
2026-05-07 19:10:04 -0400

    last try

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
             i = doFizzBuzz(i);
         }
     }
-
+// ATA deneme
     private static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
